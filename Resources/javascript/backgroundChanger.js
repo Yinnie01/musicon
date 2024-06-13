70f9ab9b65ddaff173ab96511e2f2597bae8a888
Yinnie Akenroye <155371795+Yinnie01@users.noreply.github.com>
2024-06-13 08:55:42 +0100

    Develop mobile view for Home, Contact and Store pages

--- Resources/javascript/backgroundChanger.js
+++ Resources/javascript/backgroundChanger.js
@@ -2,9 +2,11 @@
 
 let itemOne = document.getElementById("body");
 let itemTwo = document.getElementById("header");
+let itemThree = document.getElementById("footer");
 
 function changeBackground() {
     itemTwo.style.backgroundColor = "rgb(247, 93, 175)";
+    itemThree.style.backgroundColor = "rgb(247, 93, 175)";
 }
 
 itemOne.onwheel = changeBackground;
